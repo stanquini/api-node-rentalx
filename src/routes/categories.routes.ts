ff66de608d66f1e9a71affd80e30017050a7b1ab
Alfredo Stanquini Neto <stanquini@gmail.com>
2022-07-18 10:26:39 -0300

    feat(category) add create and list

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { CategoryRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from "../services/CreateCategoryService";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoryRepository();
+
+categoriesRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description })
+  return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+  return res.json(all);
+});
+
+export { categoriesRoutes };
