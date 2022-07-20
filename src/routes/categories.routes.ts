c5078a55c25066bb50d6ced0a22c5cc85d6d48c9
Alfredo Stanquini Neto <stanquini@gmail.com>
2022-07-20 16:27:33 -0300

    feat() refactory + add specifications

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,16 @@
 import { Router } from 'express';
-import { CategoryRepository } from '../repositories/CategoriesRepository';
-import { CreateCategoryService } from "../services/CreateCategoryService";
+
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description })
-  return res.status(201).send();
+   return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
-  const all = categoriesRepository.list();
-  return res.json(all);
+  listCategoriesController.handle(req, res)
 });
 
 export { categoriesRoutes };
