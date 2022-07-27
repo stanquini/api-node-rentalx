192afd8e65be4f6ea9c6da2da13fc0b707b41d76
Alfredo Stanquini Neto <stanquini@gmail.com>
2022-07-27 17:12:18 -0300

    feat(upload) add upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory/';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post("/", (req, res) => {
    return createCategoryController.handle(req, res);
@@ -12,5 +18,9 @@ categoriesRoutes.post("/", (req, res) => {
 categoriesRoutes.get("/", (req, res) => {
   listCategoriesController.handle(req, res)
 });
+
+categoriesRoutes.post("/import", upload.single("file"),(req, res) => {
+  importCategoryController.handle(req, res)
+})
 
 export { categoriesRoutes };
