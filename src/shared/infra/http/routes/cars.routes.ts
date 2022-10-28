83c7383683cfe28d3ed21cf5c535a51ecfc0b9d4
Alfredo Stanquini Neto <stanquini@gmail.com>
2022-10-28 10:08:56 -0300

    register and upload images car

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/UploadCarImagesController";
+import multer from "multer";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
+import uploadConfig from "@config/upload";
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const uploadCars = multer(uploadConfig.upload("./tmp/cars"))
 
 carsRoutes.post(
   "/",
@@ -27,6 +34,14 @@ carsRoutes.post(
   ensureAdmin,
   createCarSpecificationController.handle
 );
+
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadCars.array("images"),
+  uploadCarImagesController.handle
+)
 
 export { carsRoutes };
  
